5a7f406c38cf37ff8e23544081829db3617f7b74
unknown <arupaul1650@gmail.com>
2024-01-16 20:42:33 +0530

    Login, signup route created

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import { Box, Button, Heading, Input, Stack } from '@chakra-ui/react'
+import React from "react";
+import { Box, Button, Heading, Input, Stack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 function Signup() {
   return (
     <Box
-    maxW={{ base: "90%", md: "500px" }}
-    mx="auto"
-    mt={8}
-    p={8}
-    borderWidth="1px"
-    borderRadius="md"
-    boxShadow="lg"
-  >
-    <Heading mb={4} textAlign="center">
-      Register Now
-    </Heading>
-    <form >
-      <Stack spacing={4}>
-        <Input type="text" placeholder="Username" />
-        <Input type="password" placeholder="Password" />
-        <Button type="submit" colorScheme="blue" width="full">
-          Signup
-        </Button>
-      </Stack>
-    </form>
-  </Box>
-  )
+      maxW={{ base: "90%", md: "500px" }}
+      mx="auto"
+      mt={8}
+      p={6}
+      borderWidth="1px"
+      borderRadius="md"
+      boxShadow="lg"
+    >
+      <Heading mb={4} textAlign="center">
+        SignUp
+      </Heading>
+      <form>
+        <Stack spacing={4}>
+          <Input type="text" placeholder="Username" />
+          <Input type="password" placeholder="Password" />
+          <Button type="submit" colorScheme="blue" width="full">
+            Signup
+          </Button>
+        </Stack>
+      </form>
+      <Text fontSize="sm" mt={4}>
+        already have account | <Link to="/">Login</Link>
+      </Text>
+    </Box>
+  );
 }
 
-export default Signup
+export default Signup;
